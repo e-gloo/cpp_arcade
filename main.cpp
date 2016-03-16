6228d12cbc3c89e3c114615063beefbfacea3dc0
François CASSIN <francois.cassin@epitech.eu>
2016-03-16 15:54:09 +0100

    main

--- main.cpp
+++ main.cpp
@@ -5,16 +5,27 @@
 int	main(int ac, char **av)
 {
   void	*dlhandle;
+  void	*dlghandle;
   IDisplayManager* (*creator)();
+  IGame		 * (*gcreator)();
 
   dlhandle = dlopen(av[1], RTLD_NOW);
   if (dlhandle == NULL)
+    exit(1);
+
+  if ((dlghandle = dlopen(av[2], RTLD_NOW)) == NULL)
     exit(1);
 
   creator = reinterpret_cast<IDisplayManager * (*)()>(dlsym(dlhandle, "getInstance"));
   if (creator == NULL)
     exit(1);
   IDisplayManager *manager = creator();
+
+  gcreator = reinterpret_cast<IGame * (*)()>(dlsym(dlghandle, "getInstance"));
+  if (gcreator == NULL)
+    exit(1);
+  IGame *game = gcreator();
+
   delete manager;
   (void)ac;
 }
